f2a4a78d5a78113c782111c9d377bd2b63cca9be
RobertStojanowski <robstoja@icloud.com>
2014-07-26 20:57:39 -0400

    Updated Project2
    Additional changes

--- ProgrammingAssignment2.R
+++ ProgrammingAssignment2.R
@@ -5,7 +5,17 @@
 ##its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-  
+        m <- NULL
+          set <-function(y=matrix()){
+              x<<-y
+              m<<-NULL
+          }
+        get <- function()x
+        setmean <-function(mean) m <<-mean
+        getmean <-function()m
+        list(set=set, get=get,
+             setmean=setmean,
+             getmean=getmean)
 }
 
 
@@ -13,5 +23,17 @@ makeCacheMatrix <- function(x = matrix()) {
 ##makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
+      m <- x$getmean()
+        if(!is.null(m)){
+            message("getting cached data")
+            return(m)
+        }
+      data <- x$get()
+      m<-mean(data,...)
+      x$setmean(m)
+      m
+      
   ## Return a matrix that is the inverse of 'x'
 }
+
+cachemean([2,])
